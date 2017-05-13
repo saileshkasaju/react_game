e42320fde151c7bbd77c045b7f40df2d1df3fe9a
saileshkasaju <leosailesh@gmail.com>
2017-05-13 19:44:51 +0545

    add some css styling for game

--- src/Numbers.js
+++ src/Numbers.js
@@ -8,8 +8,8 @@ const Numbers = (props) => {
         <div className="card text-center">
             <div>
                 <span>1</span>
-                <span>2</span>
-                <span>3</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
             </div>
         </div>
     )
